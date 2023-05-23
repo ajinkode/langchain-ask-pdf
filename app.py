047d783a1fc5b5ccab9aa5d92867d868ee665cf2
Ajinkya Kale <127986789+ajinkode@users.noreply.github.com>
2023-05-23 16:40:56 +0530

    Saved the original file

--- app.py
+++ app.py
@@ -24,29 +24,29 @@ def main():
       for page in pdf_reader.pages:
         text += page.extract_text()
         
-#       # split into chunks
-#       text_splitter = CharacterTextSplitter(
-#         separator="\n",
-#         chunk_size=1000,
-#         chunk_overlap=200,
-#         length_function=len
-#       )
-#       chunks = text_splitter.split_text(text)
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
       
-#       # create embeddings
-#       embeddings = OpenAIEmbeddings()
-#       knowledge_base = FAISS.from_texts(chunks, embeddings)
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
       
-#       # show user input
-#       user_question = st.text_input("Ask a question about your PDF:")
-#       if user_question:
-#         docs = knowledge_base.similarity_search(user_question)
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
         
-#         llm = OpenAI()
-#         chain = load_qa_chain(llm, chain_type="stuff")
-#         with get_openai_callback() as cb:
-#           response = chain.run(input_documents=docs, question=user_question)
-#           print(cb)
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
            
         st.write(text)
     
